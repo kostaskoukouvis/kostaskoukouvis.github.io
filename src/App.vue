b0440f438574f5d881cc7fcef10ea16dada6fda5
Konstantinos <konstantinos.koukouvis@mecenat.com>
2024-12-11 15:05:36 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <main class="p-10">
+    <HelloWorld msg="Vite + Vue" />
+  </main>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
+</script>
+
+<template>
+  <Accordion type="single" collapsible>
+    <AccordionItem value="item-1">
+      <AccordionTrigger>Is it accessible?</AccordionTrigger>
+      <AccordionContent>
+        Yes. It adheres to the WAI-ARIA design pattern.
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/ndf3fTN0ZL0?si=qtHjX_qWK35zllET"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen></iframe>
+      </AccordionContent>
+    </AccordionItem>
+  </Accordion>
+</template>
