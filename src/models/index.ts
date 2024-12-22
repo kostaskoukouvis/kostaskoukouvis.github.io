695f3aea50a57902b7e6621dde42923802749794
Konstantinos <konstantinos.koukouvis@mecenat.com>
2024-12-22 11:57:44 +0100

    fix dependencies in models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,10 +1,8 @@
-import { CountryCode } from 'vue3-flag-icons'
-
 export interface Album {
   position: number
   artist: string
   title: string
-  country: CountryCode
+  country: string
   cover: string
   video: string
   track: string
